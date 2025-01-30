251a9fe4dd4fc4bf53b3c8ff3780960d20d7e267
Žiga Leber <mail+github@zigaleber.com>
2025-01-30 11:45:06 +0100

    Export RateLimiterStoreAbstract

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const BurstyRateLimiter = require('./lib/BurstyRateLimiter');
 const RateLimiterRes = require('./lib/RateLimiterRes');
 const RateLimiterDynamo = require('./lib/RateLimiterDynamo');
 const RateLimiterPrisma = require('./lib/RateLimiterPrisma');
+const RateLimiterStoreAbstract = require('./lib/RateLimiterStoreAbstract');
 
 module.exports = {
   RateLimiterRedis,
@@ -30,4 +31,5 @@ module.exports = {
   RateLimiterRes,
   RateLimiterDynamo,
   RateLimiterPrisma,
+  RateLimiterStoreAbstract
 };
